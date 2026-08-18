5ace9651f1053dba03237f5ebea8e703b79f8e86
sabbirba <sabbirba10@gmail.com>
2026-08-18 18:23:33 +0600

    feat(preprinttui): bump to 0.1.4

--- Formula/preprinttui.rb
+++ Formula/preprinttui.rb
@@ -1,20 +1,20 @@
 class Preprinttui < Formula
   desc "Interactive Terminal User Interface for PreConnect printer"
   homepage "https://preconnect.app"
-  version "0.1.3"
+  version "0.1.4"
   license "GPL-3.0-only"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/sabbirba/preprinttui/releases/download/v#{version}/preprinttui-macos-arm64.tar.gz"
-      sha256 "2830a73cda1f93f2b38348a56c59897a60ea836e1653993ddb07db4c9dd88a99"
+      sha256 "17d2260b9dc5188b2198f2de4e76b56acd6853add03a4c71cae5f11a0745a432"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/sabbirba/preprinttui/releases/download/v#{version}/preprinttui-linux-musl-x86_64.tar.gz"
-      sha256 "33d6e09828c8280e4ab674bc4117c1b2bf22355b9065e5d84e4f81e3691a021e"
+      sha256 "d7998c99f2a77ce9c86639c276c9caded4eb2c785a1d744bf13debcfa7f93240"
     end
   end
 
